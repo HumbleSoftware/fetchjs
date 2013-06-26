211ce53ffd087b4470ac454276ad760a931e4e12
Carl Sutherland <carl@humblesoftware.com>
2013-06-26 13:04:23 -0400

    Implement initial intercepts middlewear.
    * listen to standard in for user input
    * implement a set of commands to allow the user to intercept routes
    * provide custom responses based on intercepts.

--- examples/hello_world/spec/index.js
+++ examples/hello_world/spec/index.js
@@ -2,6 +2,7 @@ describe('/', function () {
   it('GET "Hello World!"', function (done) {
     request
       .get('/', function (req, res) {
+        expect(res.status).to.equal(200);
         expect(res.text).to.equal('Hello World!');
         done();
       })
