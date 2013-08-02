c58fffaf1425467b7d75e75689b1a0b5ce696290
Carl Sutherland <carl@humblesoftware.com>
2013-08-02 17:29:09 -0400

    Update hello world test to new API.

--- examples/hello_world/spec/index.js
+++ examples/hello_world/spec/index.js
@@ -1,10 +1,6 @@
-describe('/', function () {
-  it('GET "Hello World!"', function (done) {
-    request
-      .get('/', function (req, res) {
-        expect(res.status).to.equal(200);
-        expect(res.text).to.equal('Hello World!');
-        done();
-      })
+test.get('/', function (error, res) {
+  it('returns "Hello World!"', function () {
+    expect(res.status).to.equal(200);
+    expect(res.text).to.equal('Hello World!');
   });
 });
